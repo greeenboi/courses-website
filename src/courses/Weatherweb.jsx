dd0008b0d8ae4fd3d60f1a16a846b0f488de547b
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-07-09 02:02:11 +0530

    added code and a theme for it

--- src/courses/Weatherweb.jsx
+++ src/courses/Weatherweb.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { React ,useEffect, useRef } from 'react'
+import styled from 'styled-components'
+import hljs from "highlight.js";
+
+const Section = styled.section`
+  height: 180vh;
+  width:100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  scroll: auto;
+  justify-content: space-between;
+`;
+
+
+
+const Container = styled.div`
+  height: 100vh;
+  width: 99vw;
+  
+  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+`;
 
 const Weather = () => {
+  useEffect(() => {
+    hljs.highlightAll();
+  });
+
   return (
-    <div>Weather</div>
+    <Section>
+        <Container>
+          <div>
+          <pre>
+            <code className="language-typescript">const variable = 'raw';</code>
+          </pre>
+          </div>
+        </Container>
+    </Section>
   )
 }
 
