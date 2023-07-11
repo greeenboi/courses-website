a1268e34ffc19d14a5c842ebcd52ce06dbda838a
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-07-12 00:24:30 +0530

    added basic introduction to the website

--- src/courses/Weatherweb.jsx
+++ src/courses/Weatherweb.jsx
@@ -51,6 +51,14 @@ const Img = styled.img`
     border: 4px solid rgba(255,255,255,0.2);
   }
 `
+const Card = styled.div`
+  width:fit-content;
+  height: fit-content;
+  padding: 1.5rem;
+  margin: 0px 10px 10px 0px;
+  background-color:rgba(255,255,255,0.2);
+  border-radius: 5px;
+`
 
 const Title = styled.h1`
   background-image: linear-gradient(to top,var(--tw-gradient-stops));
@@ -75,12 +83,12 @@ const Text1 = styled.p`
   gap:15px;
   justify-content:space-between;
 `
-const Text = styled.p`
+const Text2 = styled.p`
   font-size:18px;
   padding:10px;
   font-family:'Sofia Sans', sans-serif;
   font-weight:300;
-  
+  text-align:left;
 `
 
 const Button = styled.button`
@@ -130,19 +138,28 @@ const Weather = () => {
     <Section>
         <Container>
           <Pcard>
-          <Text1><Img src="https://avatars.githubusercontent.com/u/118198968?v=4" alt="suvangs"/>By Suvan GS</Text1>
-          <Title>Weather Website with Open Weather Api</Title>  
-          <List>
-          <Samp>{`#Api`} </Samp>  
-          <Samp>{`#TailWind Css`}</Samp>  
-          </List>
-          <Button onClick={() => window.open('https://github.com/greeenboi/weather-website')}>Source Code</Button>
+            <Text1><Img src="https://avatars.githubusercontent.com/u/118198968?v=4" alt="suvangs"/>By Suvan GS</Text1>
+            <Title>Weather Website with Open Weather Api</Title>  
+            <List>
+              <Samp>{`#Api`} </Samp>  
+              <Samp>{`#TailWind Css`}</Samp>  
+            </List>
+            <Button onClick={() => window.open('https://github.com/greeenboi/weather-website')}>Source Code</Button>
           </Pcard>
-          <div>
-          <pre>
-            <code className="language-javascript">{`useEffect(() => { hljs.highlightAll(); });`}</code>
-          </pre>
-          </div>
+          <Card>
+            <Text2>
+              For this Project we will be creating a website that shows weather information on a particular city.<br />
+              We will be using the Open Weather API to get weather information on a specific city and Tailwind css to style the website.
+              <br /><br />
+              👉I will guide you through the Api key creation and help you get started with simple API operations like 'GET'and 'POST'. <br /><br />
+              👉Then I will help you setup the project as a simple Vite application with JS, Tailwind css and Publish your Website to a service provider like Netlify or Vercel.
+            </Text2>
+            <div>
+            <pre>
+              <code className="language-javascript">{`useEffect(() => { hljs.highlightAll(); });`}</code>
+            </pre>
+            </div>
+          </Card>
         </Container>
     </Section>
   )
